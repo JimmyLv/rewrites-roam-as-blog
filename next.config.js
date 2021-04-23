dfa4462ec0476e5c77b5fad0c4ab6df9bde8f8d7
JimmyLv <jimmy.jinglv@gmail.com>
2021-04-23 17:31:20 +0800

    feat: update the rewrites config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,47 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/',
+        source: '/team',
+        destination: '/about',
+      },
+      {
+        source: '/about-us',
+        destination: '/about',
+      },
+      // Path Matching - will match `/post/a` but not `/post/a/b`
+      {
+        source: '/post/:slug',
+        destination: '/news/:slug',
+      },
+      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
+      {
+        source: '/blog/:slug*',
+        destination: '/news/:slug*',
+      },
+      // Rewriting to an external URL
+      {
+        source: '/docs/:slug',
+        destination: 'http://example.com/docs/:slug',
+      },
+      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
+      {
+        source: '/myblog/:slug*',
+        destination: 'https://blog.jimmylv.info/:slug*',
+      },
+      // Rewriting to an external URL
+      {
+        source: '/roam/:slug*',
         destination: 'https://roamresearch.com',
+      },
+      // Rewriting to an external URL
+      {
+        source: '/roamcn',
+        destination: 'https://roamcn.club',
+      },
+      // Rewriting to an external URL
+      {
+        source: '/rr',
+        destination: 'https://roamresearch.com/#/app/RoamCN',
       },
     ]
   },
