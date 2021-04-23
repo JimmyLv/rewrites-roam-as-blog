6133deb1d543efe685c8990ea88c615059174995
吕立青 <jimmy.jinglv@gmail.com>
2021-04-23 17:23:49 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,26 +2,7 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/team',
-        destination: '/about',
-      },
-      {
-        source: '/about-us',
-        destination: '/about',
-      },
-      // Path Matching - will match `/post/a` but not `/post/a/b`
-      {
-        source: '/post/:slug',
-        destination: '/news/:slug',
-      },
-      // Wildcard Path Matching - will match `/blog/a` and `/blog/a/b`
-      {
-        source: '/blog/:slug*',
-        destination: '/news/:slug*',
-      },
-      // Rewriting to an external URL
-      {
-        source: '/roamcn',
+        source: '/',
         destination: 'https://roamresearch.com',
       },
     ]
